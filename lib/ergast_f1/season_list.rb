a72a312768b0076f86b6445d2b2e72e0ef57b377
Nathan Burgess <nathan@3playmedia.com>
2017-11-18 10:50:19 -0500

    Everything but finishing position

--- lib/ergast_f1/season_list.rb
+++ lib/ergast_f1/season_list.rb
@@ -1,27 +1,21 @@
 module ErgastF1
   class SeasonList
-    def self.find(circuit: nil, constructor: nil, driver_name: nil)
-      @circuit = circuit
-      @constructor = constructor
-      @driver_name = driver_name
+    def self.find(circuit: nil, constructor: nil, driver: nil, finishing_position: nil)
+      query_path = ""
+      query_path += "drivers/#{driver}/" if driver
+      query_path += "constructors/#{constructor}/" if constructor
+      query_path += "circuits/#{circuit}/" if circuit
+      query_path += "results/#{finishing_position}/" if finishing_position
 
-      return query_results
+      return query_results(query_path)
     end
 
     private
 
-    def self.query_results
+    def self.query_results(query_path)
       endpoint = query_path + "seasons"
       parsed_response = ErgastClient.new(endpoint).api_get_request
       return parsed_response.dig("MRData", "SeasonTable") || []
     end
-
-    def self.query_path
-      url_path = ""
-      url_path += "drivers/#{@driver_name}/" if @driver_name
-      url_path += "constructor/#{@constructor}/" if @constructor
-      url_path += "circuits/#{@circuit}/" if @circuit
-    end
-
   end
 end
--- spec/season_list_spec.rb
+++ spec/season_list_spec.rb
@@ -18,53 +18,60 @@ RSpec.describe ErgastF1::SeasonList do
 
   context "when supplied an invalid circuit name" do
     it "returns an empty array" do
-      season_list = ErgastF1::SeasonList.new(circuit: "RainbowRoad")
-      expect(season_list).to eq([])
+      VCR.use_cassette("season_list/rainbow_road") do
+        season_list = ErgastF1::SeasonList.find(circuit: "RainbowRoad")
+        expect(season_list["circuitId"]).to eq("rainbowroad")
+        expect(season_list["Seasons"]).to eq([])
+      end
     end
   end
 
   context "when supplied a valid constructor name" do
     it "returns all seasons a constructor competed in" do
       VCR.use_cassette("season_list/williams_seasons") do
-        season_list = ErgastF1::SeasonList.new(constructor: "williams")
+        season_list = ErgastF1::SeasonList.find(constructor: "williams")
         expect(valid_season_list?(season_list)).to be true
-        # TODO: MAYBE NOT IN HELPER
-        expect(season_list["constructor"]).to eq("williams")
+        expect(season_list["constructorId"]).to eq("williams")
       end
     end
   end
 
   context "when supplied an invalid constructor name" do
     it "returns an empty array" do
-      season_list = ErgastF1::SeasonList.new(constructor: "Racermotors")
-      expect(season_list).to eq([])
+      VCR.use_cassette("season_list/racer_motors_seasons") do
+        season_list = ErgastF1::SeasonList.find(constructor: "Racermotors")
+        expect(season_list["constructorId"]).to eq("racermotors")
+        expect(season_list["Seasons"]).to eq([])
+      end
     end
   end
 
   context "when supplied a valid driver name" do
     it "returns all seasons a driver competed in" do
       VCR.use_cassette("season_list/schumacher_seasons") do
-        season_list = ErgastF1::SeasonList.new(driver: "schumacher")
+        season_list = ErgastF1::SeasonList.find(driver: "schumacher")
         expect(valid_season_list?(season_list)).to be true
-        expect(season_list["driver"]).to eq("schumacher")
+        expect(season_list["driverId"]).to eq("schumacher")
       end
     end
   end
 
   context "when supplied an invalid driver name" do
     it "returns an empty array" do
-      season_list = ErgastF1::SeasonList.new(driver: "GoMifune")
-      expect(season_list).to eq([])
+      VCR.use_cassette("season_list/GoMifune_seasons") do
+        season_list = ErgastF1::SeasonList.find(driver: "GoMifune")
+        expect(season_list["Seasons"]).to eq([])
+      end
     end
   end
 
   context "when supplied a constructor and a driver" do
     it "returns all seasons that driver drove for that constructor" do
       VCR.use_cassette("season_list/schumacher_ferrari_seasons") do
-        season_list = ErgastF1::SeasonList.new(driver: "schumacher", constructor: "ferrari")
+        season_list = ErgastF1::SeasonList.find(driver: "schumacher", constructor: "ferrari")
         expect(valid_season_list?(season_list)).to be true
-        expect(season_list["driver"]).to eq("schumacher")
-        expect(season_list["constructor"]).to eq("ferrari")
+        expect(season_list["driverId"]).to eq("schumacher")
+        expect(season_list["constructorId"]).to eq("ferrari")
       end
     end
   end
@@ -72,8 +79,10 @@ RSpec.describe ErgastF1::SeasonList do
   context "when supplied a constructor and a driver that never drove for that constructor" do
     it "returns an empty array" do
       VCR.use_cassette("season_list/constructor_with_wrong_driver") do
-        season_list = ErgastF1::SeasonList.new(driver: "senna", constructor: "ferrari")
-        expect(season_list).to eq([])
+        season_list = ErgastF1::SeasonList.find(driver: "senna", constructor: "ferrari")
+        expect(season_list["driverId"]).to eq("senna")
+        expect(season_list["constructorId"]).to eq("ferrari")
+        expect(season_list["Seasons"]).to eq([])
       end
     end
   end
@@ -81,7 +90,8 @@ RSpec.describe ErgastF1::SeasonList do
   context "when supplied a constructor and a finishing position" do
     it "returns all seasons that that constructor finished in that position" do
       VCR.use_cassette("season_list/ferrari_titles") do
-        season_list = ErgastF1::SeasonList.new(finishing_position: 1, constructor: "ferrari")
+        season_list = ErgastF1::SeasonList.find(finishing_position: 1, constructor: "ferrari")
+        # BROKEN! 1988 WHAT?
         expect(valid_season_list?(season_list)).to be true
         expect(season_list["finishing_position"]).to eq(1)
       end
@@ -91,14 +101,9 @@ RSpec.describe ErgastF1::SeasonList do
   context "when supplied a constructor and a finishing position they never achieved" do
     it "returns an empty arry" do
       VCR.use_cassette("season_list/minardi_titles") do
-        season_list = ErgastF1::SeasonList.new(finishing_position: 1, constructor: "mindardi")
+        season_list = ErgastF1::SeasonList.find(finishing_position: 1, constructor: "mindardi")
         expect(season_list).to eq([])
       end
     end
   end
-end
-
-def valid_season_list(result)
-  # TODO IMPLEMENT
-  false
 end
